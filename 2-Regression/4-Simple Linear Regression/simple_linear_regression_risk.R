b840094f0d141ebd8ccd5c9cc3dbfce3f7255685
brokerstir <take1risk@gmail.com>
2018-02-23 21:36:32 -0500

    simple linear regression R part 4

--- 2-Regression/4-Simple Linear Regression/simple_linear_regression_risk.R
+++ 2-Regression/4-Simple Linear Regression/simple_linear_regression_risk.R
@@ -27,3 +27,30 @@ regressor = lm(formula = Salary ~ YearsExperience,
 # Predicting the Test set Results
 y_pred = predict(regressor, newdata = test_set)
 # After execution, type y_pred in console to see values
+
+# Visualising the Training set results
+# install.packages('ggplot2') // Commented out aftr install
+
+# Call library
+library(ggplot2)
+
+# Plot observation points, regression line, label axis. Points plotted with real observation data, but line with y predicted values
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Training set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+
+# Visualising the Test set results. Keep regression line oon training set, because it's a already trained on training set, using test set would obtain same line with new points.
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
