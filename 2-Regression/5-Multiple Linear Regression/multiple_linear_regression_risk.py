a4618796fdd1887701ad32ded2b43778a027aa88
brokerstir <take1risk@gmail.com>
2018-02-26 06:58:54 -0500

    multiple linear regression python part 2

--- 2-Regression/5-Multiple Linear Regression/multiple_linear_regression_risk.py
+++ 2-Regression/5-Multiple Linear Regression/multiple_linear_regression_risk.py
@@ -30,3 +30,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 # Manual feature scaling not needed
 
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
