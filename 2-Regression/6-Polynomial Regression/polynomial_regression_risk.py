d883e01cf12cc109f123746a0ec93b47498413a6
brokerstir <take1risk@gmail.com>
2018-03-07 06:04:36 -0600

    Polynomial Regression in Python Step 1

---
+++ 2-Regression/6-Polynomial Regression/polynomial_regression_risk.py
@@ -0,0 +1,27 @@
+# Polynomial Regression
+
+# Paste data preprocessing code
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values # Selects 2 col, makes X matrix
+y = dataset.iloc[:, 2].values # Selects 3 col, makes y vector
+
+# Splitting the dataset into the Training set and Test set
+# Commented out because data set is too small to split
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+# Commented out becasue Linear Regression library will take care of feature scaling.
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+# End data preprocessing
+
