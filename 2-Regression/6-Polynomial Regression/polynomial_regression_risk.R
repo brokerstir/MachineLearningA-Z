776d4e4abf1679f7ec359938a9274fe41dd4b1fe
brokerstir <take1risk@gmail.com>
2018-03-28 17:04:50 -0500

    Polynomial Regression in R Step 3

--- 2-Regression/6-Polynomial Regression/polynomial_regression_risk.R
+++ 2-Regression/6-Polynomial Regression/polynomial_regression_risk.R
@@ -27,7 +27,31 @@ lin_reg = lm(formula = Salary ~ .,
 # Fitting Polynomial Regression to the dataset
 dataset$Level2 = dataset$Level^2 # Adds a col of ind var squared
 dataset$Level3 = dataset$Level^3
-# dataset$Level4 = dataset$Level^4
+dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset) # Type summary(poly_reg) in console to see results
+
+# Visualising the Linear Regression results
+# install.packages('ggplot2')
+library(ggplot2)
+# Plot observed date in red and predictions in blue
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Linear Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+# You can see the linear reression model does not make good predictions
+
+# Visualising the Polynomial Regression results
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Polynomial Regression)') +
+  xlab('Level') +
+  ylab('Salary')
 
