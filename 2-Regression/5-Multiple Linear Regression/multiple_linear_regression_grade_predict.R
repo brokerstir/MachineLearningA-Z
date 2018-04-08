d937aaf1e54d247f9dd383da6c0049d2dcfc5511
brokerstir <take1risk@gmail.com>
2018-04-08 15:28:57 -0500

    Multiple Linear Progression Grade Predictor in R, Backward Elim

--- 2-Regression/5-Multiple Linear Regression/multiple_linear_regression_grade_predict.R
+++ 2-Regression/5-Multiple Linear Regression/multiple_linear_regression_grade_predict.R
@@ -50,32 +50,23 @@ y_pred = predict(regressor, newdata = test_set)
 
 # Type y_pred in console to look at it, and compare tith test_set
 
+# BACKWARD ELIMINATION
 
-
-
+# Set significance level SL at 5%
 # Building the optimal model using bacdward elmination
 # Use regressor model but type out all ind vars
-regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
-               data = dataset) # on whole data set for complete info
+regressor = lm(formula = Grade ~ MinutesStudy + HoursSleep + Subject,
+               data = gradesdataset) # on whole data set for complete info
 # Beauty of R is no need to create dummy vars, the factor function does this when encoding categorical data
 
 summary(regressor) # Returns data to search for vars with P value above signif level
 
 # Complete backwared elimination, remove vars with high P values, first to remove is State
-regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
-               data = dataset) 
+regressor = lm(formula = Grade ~ MinutesStudy + Subject,
+               data = gradesdataset) # on whole data set for complete info
 summary(regressor)
 
-# Next, remove Administration
-regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
-               data = dataset) 
+# Next, remove Subject
+regressor = lm(formula = Grade ~ MinutesStudy,
+               data = gradesdataset) # on whole data set for complete info
 summary(regressor)
-
-# Next, remove Marketing.Spend
-regressor = lm(formula = Profit ~ R.D.Spend,
-               data = dataset) 
-summary(regressor)
-
-
-
-
