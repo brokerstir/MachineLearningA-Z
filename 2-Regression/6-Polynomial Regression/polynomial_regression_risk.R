27ac118184570cc2466e3e15e03bec4f04a52dcd
brokerstir <take1risk@gmail.com>
2018-04-09 08:21:48 -0500

    Polynomial Regression in R, Step 4

--- 2-Regression/6-Polynomial Regression/polynomial_regression_risk.R
+++ 2-Regression/6-Polynomial Regression/polynomial_regression_risk.R
@@ -55,3 +55,12 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
+# Predicting a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Predicting a new result with Polynomial Regression
+y_poly_pred = predict(poly_reg, data.frame(Level = 6.5,
+                             Level2 = 6.5^2,
+                             Level3 = 6.5^3,
+                             Level4 = 6.5^4))
+
