2cf98b4fa7b95a9d0b981234a453c744d5a05319
brokerstir <take1risk@gmail.com>
2018-04-13 10:19:27 -0500

    Logistic Regression in Python, 5

--- 3-Classification/14-Logistic Regression/logistic_regression_risk.py
+++ 3-Classification/14-Logistic Regression/logistic_regression_risk.py
@@ -42,7 +42,7 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix # Functions start with lower case
 cm = confusion_matrix(y_test, y_pred)
 
-
+# STEP 5
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -66,14 +66,14 @@ plt.show()
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01)) # Taking min and max with padding of 1 for space around pixels and step is for resolution
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), # Applies classifier to prediction points and makes contour classifier line
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+                c = ListedColormap(('red', 'green'))(i), label = j) # Plots the data points
 plt.title('Logistic Regression (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
