240e16fe3d187baf1ed281847310d41e5dfa92e5
brokerstir <take1risk@gmail.com>
2018-04-15 12:36:09 -0500

    Logisitic Regression in R, Step 5

--- 3-Classification/14-Logistic Regression/logistic_regression_risk.R
+++ 3-Classification/14-Logistic Regression/logistic_regression_risk.R
@@ -40,22 +40,30 @@ y_pred = ifelse(prob_pred > 0.5, 1, 0)
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred > 0.5)
 
+# STEP 5
+
 # Visualising the Training set results
+#install.packages('ElemStatLearn')
 library(ElemStatLearn)
 set = training_set
+# Build grid with padding of 1
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
+# Gives names to columns
+colnames(grid_set) = c('Age', 'EstimatedSalary') 
 prob_set = predict(classifier, type = 'response', newdata = grid_set)
+# Return vector of predictions
 y_grid = ifelse(prob_set > 0.5, 1, 0)
 plot(set[, -3],
      main = 'Logistic Regression (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+# Print predicted plots and real data points
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+# Linear classifier separates regions with straight line
 
 # Visualising the Test set results
 library(ElemStatLearn)
